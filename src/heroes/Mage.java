8ddd36156a2755779767bd5c921002e21290cb53
Curs <oriana.pavel08@gmail.com>
2018-11-20 21:03:26 +0200

    a

---
+++ src/heroes/Mage.java
@@ -0,0 +1,19 @@
+package heroes;
+
+public class Mage extends Hero {
+    public Mage(Integer energy, Integer life) {
+        super(energy, life);
+    }
+
+    @Override
+    public void takeDamage() {
+        life --;
+        energy ++;
+    }
+
+    @Override
+    public void attack() {
+        energy -= 3;
+    }
+
+}
---
+++ src/person/Child.java
@@ -0,0 +1,15 @@
+package person;
+
+public class Child extends Person {
+    public Child(String name, Integer energy) {
+        super(name, energy);
+    }
+    public void eat(){
+        energy++;
+    }
+
+    @Override
+    public void sleep() {
+        energy = 50;
+    }
+}
